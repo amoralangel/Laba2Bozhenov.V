8d722ffc1d986bb4eeb8847e33292c2c17a0c5b9
amoralangel <71276778+amoralangel@users.noreply.github.com>
2020-09-21 17:02:57 +0300

    Add files via upload

---
+++ laba2.2bozhenov/src/laba22/BallTest.java
@@ -0,0 +1,16 @@
+package laba22;
+
+public class BallTest {
+    public static void main(String[] args) {
+        Ball b1 = new Ball(100, 100);
+        System.out.println(b1);
+        b1.move(30, 15);
+        System.out.println(b1);
+
+
+
+    }
+
+}
+
+
