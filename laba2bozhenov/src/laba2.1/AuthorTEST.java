f37fd200c447723f7dea545b681a51ad3477a3e9
amoralangel <71276778+amoralangel@users.noreply.github.com>
2020-09-21 16:56:17 +0300

    Add files via upload

---
+++ laba2bozhenov/src/laba2.1/AuthorTEST.java
@@ -0,0 +1,8 @@
+package laba2;
+
+public class AuthorTEST {
+    public static void main(String[] args) {
+        Author a1= new Author("Bozhenov", "acck.n@yandex.ru", 'M');
+        System.out.println(a1);
+    }
+}
